d2076c387801f27c84bb989dd249fa2834362759
Roger Carrijo <rogerhcp@gmail.com>
2020-01-11 15:51:25 -0300

    solve bug tagId

--- Pn532NfcReader/Pn532NfcReader.cpp
+++ Pn532NfcReader/Pn532NfcReader.cpp
@@ -36,9 +36,18 @@ ReadError  Pn532NfcReader::read(Pnrd* pnrd){
 						
 						int uidLength = tag.getUidLength();
 						byte uid[uidLength];
-						tag.getUid(uid, uidLength);
+						tag.getUid(uid, uidLength);					
+
+						uint32_t bignum = 0;
+						String charTagId ;
+						for(uint8_t index = 0; index < uidLength; index++){
+							charTagId += String(uid[index],HEX);
+						}
+						Serial.println(charTagId);
+						bignum = strtoul(charTagId.c_str(), NULL, 16);
+						Serial.println(bignum,HEX);
 						//Stores only the two last hex values of the uid as the tag id;
-						pnrd->setTagId(uid[uidLength]);	
+						pnrd->setTagId(bignum);	
 				
 						return getInformation( payload, pnrd);
 				}				
